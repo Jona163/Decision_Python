610b7209abcdb2da3ee3e33978fa379172a73677
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-11-27 11:15:12 -0600

    Update main.py

--- main.py
+++ main.py
@@ -4,3 +4,5 @@ from collections import Counter
 
 class Node:
     def __init__(self, feature=None, threshold=None, left=None, right=None, *, value=None):
+        self.feature = feature
+        self.threshold = threshold
