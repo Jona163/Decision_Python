7a706be3d062b3856149f7c0876c91100ef19855
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-11-27 11:15:42 -0600

    Update main.py

--- main.py
+++ main.py
@@ -12,3 +12,6 @@ class Node:
 
     def is_leaf_node(self):
         return self.value is not None
+
+class DecisionTree:
+    def __init__(self, min_samples_split=2, max_depth=100, n_features=None):
